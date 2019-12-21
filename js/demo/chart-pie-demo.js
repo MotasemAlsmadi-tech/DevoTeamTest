c8ad23a66191838037327b37ca4a658aa7743094
MotasemSmadi <31311215+MotasemSmadi@users.noreply.github.com>
2019-12-21 14:39:44 +0300

    dashbord finish

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -1,5 +1,8 @@
-// Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+// Set new default font family and font color to mimic Bootstrap's default
+// styling
+Chart.defaults.global.defaultFontFamily = 'Nunito',
+'-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Ar' +
+  'ial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
 // Pie Chart Example
@@ -7,13 +10,23 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
-    datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
+    labels: [
+      "New", "Pending", "Resolved", "Rejected"
+    ],
+    datasets: [
+      {
+        data: [
+          17, 5, 3, 8
+        ],
+        backgroundColor: [
+          '#1572ab', '#f6c23e', '#1cc88a', '#e74a3b'
+        ],
+        hoverBackgroundColor: [
+          '#3b7da5', '#f9d476', '#5ac09b', '#e46a5f'
+        ],
+        hoverBorderColor: "rgba(234, 236, 244, 1)"
+      }
+    ]
   },
   options: {
     maintainAspectRatio: false,
@@ -25,11 +38,11 @@ var myPieChart = new Chart(ctx, {
       xPadding: 15,
       yPadding: 15,
       displayColors: false,
-      caretPadding: 10,
+      caretPadding: 10
     },
     legend: {
       display: false
     },
-    cutoutPercentage: 80,
-  },
+    cutoutPercentage: 80
+  }
 });
